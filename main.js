ac8f330724c93f24fb1f2e9e8b5988e665e0b448
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-04-27 16:26:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var menu_list_array = [
+    "Chicken Tandoori Pizza",
+    "Veg Supreme Pizza",
+    "Panner Tikka Pizza",
+    "Deluxe Veggie Pizza",
+    "Veg Extravaganza Pizza"
+];
+
+function getmenu(){
+    var htmldata;
+    htmldata = "<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+    console.log(htmldata);
+}

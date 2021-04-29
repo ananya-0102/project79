a3ca9c9230f713d6bc5cfbc19515a8d70e6eaca0
ananya-0102 <ananya.0102@gmail.com>
2021-04-29 16:09:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,12 +8,13 @@ var menu_list_array = [
 
 function getmenu(){
     var htmldata;
-    htmldata = "<ol class='menulist'>"
+    htmldata = "<ol class='menulist'>";
     menu_list_array.sort();
-    for(var i=0;i<menu_list_array.length;i++){
-    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    for(var i=0; i<menu_list_array.length; i++){
+     htmldata = htmldata+'<li>' + menu_list_array[i] + '</li>';
     }
-    htmldata=htmldata+"</ol>"
-    document.getElementById("display_menu").innerHTML = htmldata;
+    htmldata = htmldata+"</ol>";
     console.log(htmldata);
+    document.getElementById("display_menu").innerHTML = htmldata;
+    
 }
